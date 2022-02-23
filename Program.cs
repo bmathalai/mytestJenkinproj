abc9bd18758c8b8d680e7c92279741f5e4f898c1
Balasubramanian Mathalai <868738@corpaa.aa.com>
2022-02-23 14:06:03 +0530

    tst

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace WMISample
             ManagementScope scope = new ManagementScope(@"\\IN-PF2S40JT\root\cimv2", con);
             scope.Connect();
 
-            // Get the WMI class path
+            // Get the WMI class path tst
             ManagementPath p =
                 new ManagementPath(
                 "\\\\IN-PF2S40JT\\root" +
